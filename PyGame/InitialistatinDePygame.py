4a366129e8dba912e888b8511261e305248877f7
thinkercat <darklevel@laposte.net>
2022-09-22 14:15:06 +0200

    UPDATE InitialisationDePygame

--- PyGame/InitialistatinDePygame.py
+++ PyGame/InitialistatinDePygame.py
@@ -1,3 +1,4 @@
+
 from operator import truediv
 import pygame as pg
 from pygame.locals import *
@@ -5,14 +6,18 @@ from pygame.locals import *
 pg.init()
 
 # Génération de la fenetre
-pg.display.set_caption("Game")           # Définir le titre (et une icone(titre,fichier_icone))de la fenetre 
-display = pg.display.set_mode((1080,720))  #Dimensions de la fenetre
-display.fill([000,000,000])
-pg.display.flip
+pg.display.set_caption("Game")             # Définir le titre de la fenetre 
+height = 100
+widht = 256
+screen = pg.display.set_mode((widht,height))   #Dimensions de la fenetre
+screen.fill([000,000,000])
+pg.display.flip()
+
 
 
 run = True 
 while run: #Tant que run = true le jeu marche
+
 
     for event in pg.event.get():   # dans la liste des evenements .get
         if event.type == pg.QUIT:   # si l'evenement pg.QUIT est activé
