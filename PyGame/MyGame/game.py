c9640e260b4bedbed7dc12a5a25c60083f7f143c
xX-DARKLEVEL-Xx <darklevel_contact@protonmail.com>
2022-09-23 18:19:13 +0200

    UPDATE game.py

--- PyGame/MyGame/game.py
+++ PyGame/MyGame/game.py
@@ -4,6 +4,8 @@ from pygame.locals import *
 
 pg.init()
 
+
+
 # Génération de la fenetre
 icon = pg.image.load('NSI\PyGame\MyGame\Assets\gameboy_icon.png')
 pg.display.set_icon(icon)
@@ -11,8 +13,9 @@ pg.display.set_caption("Game")             # Définir le titre de la fenetre
 height = 720
 widht = 1080
 screen = pg.display.set_mode((widht,height))   #Dimensions de la fenetre
-screen.fill([000,000,000])
+screen.fill([221, 190, 169])
 pg.display.flip()
+
 
 pg.mixer.music.load('NSI\PyGame\MyGame\Assets\Musiques\chill.mp3')
 pg.mixer.music.play(loops = -1)
@@ -21,9 +24,9 @@ pg.mixer.music.play(loops = -1)
 run = True 
 while run: #Tant que run = true le jeu marche
 
+    
     MousePosition = pg.mouse.get_pos()
     
-
 
     for event in pg.event.get():   # dans la liste des evenements .get
         if event.type == pg.QUIT:   # si l'evenement pg.QUIT est activé
