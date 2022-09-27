6459ffaa4a63c4f9f86622b62b7821da3b49b01f
xX-DARKLEVEL-Xx <darklevel_contact@protonmail.com>
2022-09-27 19:17:36 +0200

    ADD AnimCarresColorLine

---
+++ PyGame/carresColorLine.py
@@ -0,0 +1,40 @@
+
+from operator import truediv
+from turtle import position
+import pygame as pg
+from pygame.locals import *
+import random
+
+pg.init()
+
+# Génération de la fenetre
+pg.display.set_caption("ColorLine")             # Définir le titre de la fenetre 
+height = 50
+widht = 50*20
+screen = pg.display.set_mode((widht,height))   #Dimensions de la fenetre
+screen.fill([000,000,000])
+pg.display.flip()
+
+rectangle = [0,0,50,50] # [position de la gauche, position du haut, largeur, hauteur] => "Rect" type from pygame
+
+for carres in range(20):
+    
+    color = [random.randint(0,255),random.randint(0,255),random.randint(0,255)]
+    
+    pg.draw.rect(screen,(color),(rectangle))
+    rectangle[0] += 50
+    
+
+    
+
+pg.display.flip()
+
+
+
+run = True 
+while run: #Tant que run = true le jeu marche
+
+    for event in pg.event.get():   # dans la liste des evenements .get
+        if event.type == pg.QUIT:   # si l'evenement pg.QUIT est activé
+            run = False             # on arrete le jeu
+            pg.quit                 # et on quitte pygame
