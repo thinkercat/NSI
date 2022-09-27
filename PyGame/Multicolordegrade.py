ef78578195e373b7287d16dbcca8fd4b42b7347f
xX-DARKLEVEL-Xx <darklevel_contact@protonmail.com>
2022-09-27 19:43:17 +0200

    ADD MulticolorDegrade.py

---
+++ PyGame/Multicolordegrade.py
@@ -0,0 +1,26 @@
+
+from operator import truediv
+import pygame as pg
+from pygame.locals import *
+
+pg.init()
+
+# Génération de la fenetre
+pg.display.set_caption("(。_。)")             # Définir le titre de la fenetre 
+height = 256
+widht = 256
+screen = pg.display.set_mode((widht,height))   #Dimensions de la fenetre
+
+screen.fill([000,000,000])
+pg.display.flip()
+
+
+
+run = True 
+while run: #Tant que run = true le jeu marche
+
+
+    for event in pg.event.get():   # dans la liste des evenements .get
+        if event.type == pg.QUIT:   # si l'evenement pg.QUIT est activé
+            run = False             # on arrete le jeu
+            pg.quit                 # et on quitte pygame
