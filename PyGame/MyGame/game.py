66c00fa9665ebf85749a1ece605032c6029b2316
thinkercat <darklevel@laposte.net>
2022-10-04 09:50:22 +0200

    UPDATE game.py movement

--- PyGame/MyGame/game.py
+++ PyGame/MyGame/game.py
@@ -6,7 +6,7 @@ import random
 pg.init()
 
 # Génération de la fenetre
-icon = pg.image.load('NSI/PyGame/MyGame/Assets/gameboy.png')
+icon = pg.image.load('PyGame/MyGame/Assets/gameboy.png')
 
 pg.display.set_icon(icon)
 pg.display.set_caption("Game")             # Définir le titre de la fenetre 
@@ -16,15 +16,14 @@ screen = pg.display.set_mode((width,height))   #Dimensions de la fenetre
 screenBackground = (200,160,160)
 
 
-pg.mixer.music.load('NSI\PyGame\MyGame\Assets\Musiques\chill.mp3')
-pg.mixer.music.play(loops = -1)
+#pg.mixer.music.load('MyGame\Assets\Musiques\chill.mp3')
+#pg.mixer.music.play(loops = -1)
 
 #Dimensions jouables
 playScreenHeight = 900
 playScreenWidht = 900
-
 ## Player variables
-player = pg.image.load('NSI/PyGame/MyGame/Assets/gameboy.png')
+player = pg.image.load('PyGame/MyGame/Assets/gameboy.png')
 playerRect = player.get_rect()
 playerWidth = player.get_width()
 playerHeight = player.get_height()
@@ -34,7 +33,7 @@ playerRect.x = playScreenWidht/2
 playerRect.y = playScreenHeight/2
 
 ## Battery variables
-battery = pg.image.load('NSI/PyGame/MyGame/Assets/pile-verte.png')
+battery = pg.image.load('PyGame/MyGame/Assets/pile-verte.png')
 batteryRect = battery.get_rect()
 #Battery pos on start
 batteryRect.x = 300
@@ -43,16 +42,18 @@ batteryRect.y = 300
 ## BadBattery variable
 badBattery = []
 badBatteryRect = [] 
-badBatterySpawn = 3
+badBatterySpawn = 2
 def badBatteryGeneration(nombreDeBadBattery:int):      
     for nbOfBadBattery in range(nombreDeBadBattery):
-        badBattery.append(pg.image.load('NSI/PyGame/MyGame/Assets/pile-rouge.png'))
+        badBattery.append(pg.image.load('PyGame/MyGame/Assets/pile-rouge.png'))
         badBatteryRect.append(badBattery[nbOfBadBattery].get_rect()) 
 def badBatteryRandomSpawn():
     for nbOfBadBattery in range(len(badBatteryRect)):
         badBatteryRect[nbOfBadBattery].x = random.randint(0, playScreenWidht)
         badBatteryRect[nbOfBadBattery].y = random.randint(height-playScreenHeight, playScreenHeight)
-
+        while badBatteryRect[nbOfBadBattery].colliderect(playerRect) or badBatteryRect[nbOfBadBattery].colliderect(batteryRect):
+            badBatteryRect[nbOfBadBattery].x = random.randint(0, playScreenWidht)
+            badBatteryRect[nbOfBadBattery].y = random.randint(height-playScreenHeight, playScreenHeight)           
 ## Energy(health) variables
 maxEnergy = 150
 playerEnergy = 10
@@ -69,21 +70,17 @@ energyBarColor = [255,0,0]
 run = True 
 while run: #Tant que run = true le jeu marche
     screen.fill(screenBackground)
+    playableScreen = pg.draw.rect(screen,(0,0,0),(0,100,playScreenWidht,playScreenHeight),10)
 
-
-# Energy Spawn
-    if batteryRect.colliderect(playerRect):
+# When player collide
+    if playerRect.colliderect(batteryRect):
         batteryRect.x = random.randint(0,playScreenWidht)
         batteryRect.y = random.randint(height-playScreenHeight, playScreenHeight)
         badBatteryGeneration(badBatterySpawn)
         badBatteryRandomSpawn()
         playerEnergy += batteryEnergy
-        playerVelocity += 2
-        badBatterySpawn +=3
+        playerVelocity += 5
 
-
-
-# BadEnergy Spawn
 
 
 
@@ -112,22 +109,22 @@ while run: #Tant que run = true le jeu marche
     pg.draw.rect(screen, (0,0,0),(width-180,20,150,30), 5)
 
 # Player Movement
-    userKeyInput = pg.key.get_pressed()
-    if userKeyInput[pg.K_LEFT]:
-        if playerRect.x > 0:
-            playerRect.x -= playerVelocity
-    if userKeyInput[pg.K_RIGHT]:
-        if playerRect.x < playScreenWidht - playerWidth:
-            playerRect.x += playerVelocity
-    if userKeyInput[pg.K_UP]:
-        if playerRect.y > height-playScreenHeight:
-            playerRect.y -= playerVelocity
-    if userKeyInput[pg.K_DOWN]:
-        if playerRect.y < height - playerHeight:
-            playerRect.y += playerVelocity    
+#    userKeyInput = pg.key.get_pressed()
+#    if userKeyInput[pg.K_LEFT]:
+#        if not playerRect.y.colliderect(playableScreenRect):
+#            playerRect.x -= playerVelocity
+#    if userKeyInput[pg.K_RIGHT]:
+#        if playerRect.x - playerVelocity < playScreenWidht - playerWidth:
+#            playerRect.x += playerVelocity
+#    if userKeyInput[pg.K_UP]:
+#       if playerRect.y + playerVelocity > height-playScreenHeight:
+#            playerRect.y -= playerVelocity
+#    if userKeyInput[pg.K_DOWN]:
+#        if playerRect.y - playerVelocity < height - playerHeight:
+#            playerRect.y += playerVelocity    
 
-
-
+    MousePos = pg.mouse.get_pos()
+    playerRect = MousePos
 
 
     for event in pg.event.get():   # dans la liste des evenements .get
@@ -135,10 +132,11 @@ while run: #Tant que run = true le jeu marche
             run = False             # on arrete le jeu
             pg.quit                 # et on quitte pygame
     
-    pg.time.delay(30)               # "fps"
+    pg.time.delay(60)               # "fps"
     screen.blit(battery,batteryRect)
     for nbOfBadBattery in range(len(badBatteryRect)):
         screen.blit(badBattery[nbOfBadBattery],badBatteryRect[nbOfBadBattery])
     screen.blit(player,playerRect)
+
     
     pg.display.flip()
