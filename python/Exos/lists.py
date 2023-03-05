58d118540b4075fb9a70db12f7e4efaa87a6eb4d
xX-DARKLEVEL-Xx <darklevel_contact@protonmail.com>
2023-03-05 10:54:31 +0100

    none

--- python/Exos/lists.py
+++ python/Exos/lists.py
@@ -15,10 +15,20 @@ def recherche(tab:list,n):
 #print(recherche([1,3,4],2))
 #print(recherche([1,2,3,4,2],2))
 
-for bb in range(1,1001):
-    for br in range(bb,1001):
-        for bn in range(br,(bb*2)):
-            if (bb+br+bn)*777 == bb*br*bn:
-                print(bb,br,bn)
-                print(bb+br+bn)
-                quit()
+# for bb in range(1,1001):
+#     for br in range(bb,1001):
+#         for bn in range(br,(bb*2)):
+#             if (bb+br+bn)*777 == bb*br*bn:
+#                 print(bb,br,bn)
+#                 print(bb+br+bn)
+#                 quit()
+
+def test(h):
+    return h
+
+l = [1,2]
+
+f = [1,2]
+
+if l == [1,2]:
+    print('work')
