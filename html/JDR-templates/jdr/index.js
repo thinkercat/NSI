5813fb81e677193c74d29ed47a7dc8daa9be5fbe
xX-DARKLEVEL-Xx <darklevel_contact@protonmail.com>
2023-03-07 20:12:39 +0100

    change templates

---
+++ html/JDR-templates/jdr/index.js
@@ -0,0 +1,8 @@
+
+function changeHistoryScreen(){
+    console.log('change background')
+    document.body.style.backgroundImage = "url(./futurist_city.png)"
+}
+window.addEventListener('click', ()=>{
+    changeHistoryScreen()
+})
