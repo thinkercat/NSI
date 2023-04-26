f7ee72fa3baf9b0f1b6a728cea49197a7db4d0b3
thinkercat <darklevel@laposte.net>
2023-04-26 11:55:25 +0200

    26/04/2023

---
+++ python/Exos/tri_carte.py
@@ -0,0 +1,15 @@
+carte = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+
+def tri_selection(tab: list) -> None:
+    n = len(tab)
+    for i in range(len(tab)):
+        indice_min = tab[0]
+        for j in range( indice_min, len(tab)):
+            if tab[j] < tab[indice_min]:
+                indice_min = tab[j]
+        #il reste à échanger les valeurs d'indice i et indice_min
+        tab[indice_min], tab[i] = tab[i], tab[indice_min]
+    
+    return tab
+print(tri_selection([2,3,1]))
+assert  tri_selection([2,3,1]) == [1,2,3]
