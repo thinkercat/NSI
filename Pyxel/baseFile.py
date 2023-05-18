c17b422907094d155fbaddf87365e26e2787f5fc
xX-DARKLEVEL-Xx <darklevel_contact@protonmail.com>
2023-05-18 12:11:51 +0200

    create an pyxel example file

---
+++ Pyxel/baseFile.py
@@ -0,0 +1,88 @@
+# https://github.com/kitao/pyxel/blob/main/docs/README.fr.md
+import pyxel as pxl
+import random as rd
+
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 300
+
+class Props:
+    def __init__(self) -> None:
+        self.x = 0
+        self.y = 0
+    
+    def resetPos(self):
+        self.x = rd.randint(0,WINDOW_WIDTH-8)
+        self.y = rd.randint(0,WINDOW_HEIGHT-8)
+
+    def update(self):
+        if self.x < WINDOW_WIDTH-8:
+            self.x += 1
+        else:
+            self.x = 0
+        if self.y < WINDOW_HEIGHT-8:
+            self.y += 1
+        else:
+            self.y = 0
+
+    def draw(self):
+        pxl.blt(self.x,self.y,0,0,0,8,8)
+
+class Player:
+    def __init__(self) -> None:
+        self.x = 0
+        self.y = 0
+        self.velocity = 1
+
+    def movement(self):
+        if pxl.btn(pxl.KEY_RIGHT) and self.x < WINDOW_WIDTH:
+            self.x += self.velocity
+        if pxl.btn(pxl.KEY_LEFT) and self.x > 0:
+            self.x -= self.velocity
+        if pxl.btn(pxl.KEY_DOWN) and self.y < WINDOW_HEIGHT:
+            self.y += self.velocity
+        if pxl.btn(pxl.KEY_UP) and self.y>0:
+            self.y -= self.velocity
+
+    def update(self):
+        self.movement()
+
+    def draw(self):
+        pxl.blt(self.x,self.y,0,8,0,8,8)
+
+class Game:
+    def __init__(self) -> None:
+        pxl.init(WINDOW_WIDTH,WINDOW_HEIGHT,"Game title")
+        pxl.load("res.pyxres")
+        
+        self.player = Player()
+        self.props01 = Props()
+
+
+
+        self.score = 0
+
+        pxl.run(self.update, self.draw)
+
+    def schowScore(self):
+        f_score = f'{self.score}'
+        pxl.text(1,1,f_score,6)
+
+    def isEat(self):
+        if self.props01.x >= self.player.x and self.props01.x <= self.player.x+8 and self.props01.y >= self.player.y and self.props01.y <= self.player.y+8:
+            self.score += 1
+            self.player.velocity += 1
+            self.props01.resetPos()
+
+    def update(self):
+        self.props01.update()
+        self.player.update()
+        self.isEat()
+
+
+    def draw(self):
+        pxl.cls(0) # Efface le contenu déjà présent
+        self.props01.draw()
+        self.player.draw()
+        self.schowScore()
+
+Game()
--- python/Exos/lists.py
+++ python/Exos/lists.py
@@ -32,3 +32,25 @@ f = [1,2]
 
 if l == [1,2]:
     print('work')
+
+def ajoute_dictionnaires(d1:dict,d2:dict)->dict:
+    """
+    Fusionne deux dictionnaires de types {int:int}
+    """
+    d = {}
+    #keys = set(d1.keys()) | set(d2.keys())
+    keys = [k for k in d1.keys()] + [k for k in d2.keys()]
+
+    for key in keys:
+        if key in d1 and key in d2:
+            d[key] = d1[key] + d2[key] 
+        elif key in d1:
+            d[key] = d1[key] 
+        else:
+            d[key] = d2[key]
+
+    return d
+
+assert ajoute_dictionnaires({1: 5, 2: 7}, {2: 9, 3: 11}) == {1: 5, 2: 16, 3: 11}
+assert ajoute_dictionnaires({}, {2: 9, 3: 11}) == {2: 9, 3: 11}
+assert ajoute_dictionnaires({1: 5, 2: 7}, {}) == {1: 5, 2: 7}
